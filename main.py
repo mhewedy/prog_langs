59b03c1a7f5dc8c6f577fd1ca2fafb98ba865091
mhewedy <mhewedy@elm.sa>
2022-03-14 15:51:27 +0300

    enhance the algo

--- main.py
+++ main.py
@@ -14,36 +14,40 @@ def get_lang(lang_url):
     with request.urlopen(base_url + lang_url) as response:
         html = response.read()
         tree = etree.HTML(html)
-        print(lang_url)
-        get_influenced(tree)
-        get_influenced_by(tree)
+        influenced = get_influenced(tree)
+        influenced_by = get_influenced_by(tree)
+
+        print("language: " + lang_url +
+              " ,influenced: " + str(influenced) +
+              " ,influenced_by: " + str(influenced_by))
 
 
 def get_influenced(tree):
-    get_xpath_by_text(tree, 'Influenced')
+    return get_xpath_by_text(tree, 'Influenced')
 
 
 def get_influenced_by(tree):
-    get_xpath_by_text(tree, 'Influenced by')
+    return get_xpath_by_text(tree, 'Influenced by')
 
 
 def get_xpath_by_text(tree, text):
     try:
         influenced = tree.xpath(f"//*[text() = '{text}']")[0]
-        influenced_langs = influenced.getparent().getnext().getchildren()[0].getchildren()
+        influenced_langs = influenced.getparent().getnext().findall(".//a")
 
         influenced_lang_urls = []
 
         for influenced_lang in influenced_langs:
-            if influenced_lang.text is not None:
+            if influenced_lang.getparent().tag != 'sup' and influenced_lang.text is not None:
                 new_lang_url = influenced_lang.get("href")
                 influenced_lang_urls.append(new_lang_url)
 
-        for ll in influenced_lang_urls:
-            get_lang(ll)
-    except Exception as e:
-        # print(e)
-        pass
+        # for ll in influenced_lang_urls:
+        #     get_lang(ll)
+
+        return influenced_lang_urls
+    except:
+        return []
 
 
 get_lang('/wiki/Python_(programming_language)')
